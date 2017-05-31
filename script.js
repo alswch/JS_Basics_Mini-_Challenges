a4b1e73bd2440ca332e657145e44be5f25ce8f4e
alswch <alswchung@gmail.com>
2017-05-31 17:59:31 -0400

    created multidimensional array

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ console.log("addition", test + test2);
 var name = "Hello";
 var name2 = "World";
 console.log("name:", name, name2);
+
+var movies = ["Star Wars", "MCU", "Gladiator"];
+var food = ["Korean", "Japanese", "Italian"];
+var fav = [movies, food];
+console.log("I like:", fav[0][0]);
+console.log("I like", fav[1][0]);
