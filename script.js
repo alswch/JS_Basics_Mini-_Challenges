5f12750ad7b01a83593b69d76e93d5225266a457
alswch <alswchung@gmail.com>
2017-05-31 18:38:17 -0400

    alerted less than 100, more than 100 message.

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
+// number 1-1
 var test = 5;
 var test2 = 4;
 console.log("addition", test + test2);
 
+// number 1-2
 var name = "Hello";
 var name2 = "World";
 console.log("name:", name, name2);
 
+// number 2
 var movies = ["Star Wars", "MCU", "Gladiator"];
 var food = ["Korean", "Japanese", "Italian"];
 var fav = [movies, food];
 console.log("I like:", fav[0][0]);
-console.log("I like", fav[1][0]);
+console.log("I like", fav[1][0] + " food");
+
+// number 3
+var chosenNumber = "107";
+alert = "You have chose number less than 100."
+alert_2 = "You have chose number more than 100."
+if (chosenNumber < 100){
+  console.log(alert);
+} else {
+  console.log(alert_2);
+}
+
+// number 8
